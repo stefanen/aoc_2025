31ca319f12e82d3af61b404eb5a5216514d3c818
Stefan Engström <stefaneng86@gmail.com>
2025-12-07 14:35:55 +0100

    python3.10 cache d7

--- d7.py
+++ d7.py
@@ -1,3 +1,5 @@
+from functools import cache
+
 day = '7'
 input = open('./input_d_' + day + '.txt').read()
 lines=[e for e in input.split('\n')]
@@ -5,27 +7,15 @@ col_length=len(lines)-1
 row_length=max([len(lines[c]) for c in range(0,col_length)])
 matrix=[[list(lines[y])[x] for x in range(0,row_length)] for y in range(0,col_length)]
 
-def memoize(fn):
-    cache = {}
-
-    def wrapper(*args):
-        if args in cache:
-            return cache[args]
-        result = fn(*args)
-        cache[args] = result
-        return result
-    return wrapper
-
-
 def get_start_indices(matrix):
     for y in range(0,col_length):
         for x in range(0,row_length):
             if matrix[y][x]=='S':
                 return (y,x)
 
-@memoize
+@cache
 def sol(y,x):
-    if y>col_length-2:
+    if y>=col_length-1:
         return 1
     if matrix[y+1][x]=='.':
         return sol(y+1,x)
